724473039794c10b31ba904656c5b1f1bf5997f3
Shen <863461783@qq.com>
2024-02-09 09:15:13 +0800

    Initial Commit

---
+++ CharRain/CharRainApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharRainApp.swift
+//  CharRain
+//
+//  Created by 香饽饽zizizi on 2024/2/9.
+//
+
+import SwiftUI
+
+@main
+struct CharRainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
